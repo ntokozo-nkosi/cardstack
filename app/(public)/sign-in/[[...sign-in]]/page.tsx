4307dbfd4cc74a12a59102873f767a40430ce889
Ntokozo Nkosi <ntokozo.nkosi247@gmail.com>
2025-12-16 12:26:27 +0200

    Add forced redirect to `/decks` for sign-in and sign-up pages.

--- app/(public)/sign-in/[[...sign-in]]/page.tsx
+++ app/(public)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className="flex items-center justify-center min-h-[calc(100vh-4rem)] p-4">
-            <SignIn />
+            <SignIn forceRedirectUrl="/decks" />
         </div>
     )
 }
--- app/(public)/sign-up/[[...sign-up]]/page.tsx
+++ app/(public)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function Page() {
     return (
         <div className="flex items-center justify-center min-h-[calc(100vh-4rem)] p-4">
-            <SignUp />
+            <SignUp forceRedirectUrl="/decks" />
         </div>
     )
 }
