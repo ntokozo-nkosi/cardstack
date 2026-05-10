84c5f52748c88784a2d1562d6849fd44efdcd175
Ntokozo Nkosi <ntokozo.nkosi257@gmail.com>
2026-05-10 15:44:35 +0200

    Initial Commit

---
+++ CardStackIOS/CardStackIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardStackIOSApp.swift
+//  CardStackIOS
+//
+//  Created by Ntokozo Nkosi on 2026/05/10.
+//
+
+import SwiftUI
+
+@main
+struct CardStackIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
